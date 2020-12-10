faace92004bab47318324a52ecb3bc0093f6900a
Rachel <rachel.rolon@gmail.com>
2020-12-09 21:52:33 -0600

    Set up Events view and controller

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+
+        //Get: /Events
+        [HttpGet]
+        [Route("/events")]
+        public IActionResult Index()
+        {
+            Events.Add("Women in Tech Kansas City Event");
+            Events.Add("GodSpeed! You Black Emperor Concert"); 
+            Events.Add("League of Legends Virtual");
+
+            ViewBag.events = Events;
+            return View();
+        }
+    }
+}
