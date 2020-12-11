6e34e656e48830f0f835099a5915a6cc0c2b0ae7
Rachel <rachel.rolon@gmail.com>
2020-12-10 20:37:53 -0600

    create model class

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -2,26 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+        static private List<Event> Events = new List<Event>();
 
         //Get: /events
         [HttpGet]
         [Route("/events")]
         public IActionResult Index()
         {
-            //Events.Add("Women in Tech Kansas City Event");
-            //Events.Add("GodSpeed! You Black Emperor Concert"); 
-            //Events.Add("League of Legends Virtual");
-            
-
             ViewBag.events = Events;
-
             return View();
         }
 
@@ -37,10 +32,9 @@ namespace CodingEvents.Controllers
         //handles the form submission
         [HttpPost]
         [Route("/events/add")]
-        public IActionResult NewEvent(string name, string description)
+        public IActionResult NewEvent(string name)
         {
-            Events.Add(name, description);
-
+            Events.Add(new Event(name));
             return Redirect("/Events");
         }
     }
