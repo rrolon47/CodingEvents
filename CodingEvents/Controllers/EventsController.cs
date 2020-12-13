8a461692fbaeaad5d832c3a2e8ce861769ec1690
Rachel <rachel.rolon@gmail.com>
2020-12-12 19:20:29 -0600

    added edit feature

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -13,7 +13,7 @@ namespace CodingEvents.Controllers
 
         //Get: /events
         [HttpGet]
-        [Route("/events")]
+        [Route("/Events")]
         public IActionResult Index()
         {
             ViewBag.events = EventData.GetAll();
@@ -22,7 +22,7 @@ namespace CodingEvents.Controllers
 
         //Get: /events/add
         [HttpGet]
-        [Route("/events/add")]
+        [Route("/Events/Add")]
         public IActionResult Add()
         {
             return View();
@@ -31,14 +31,14 @@ namespace CodingEvents.Controllers
         //post: /events/add
         //handles the form submission
         [HttpPost]
-        [Route("/events/add")]
+        [Route("/Events/Add")]
         public IActionResult NewEvent(Event newEvent)
         {
             EventData.Add(newEvent);
             return Redirect("/Events");
         }
 
-        //get: /delete
+        //get: events/delete
         [HttpGet]
         public IActionResult Delete()
         {
@@ -56,5 +56,28 @@ namespace CodingEvents.Controllers
 
             return Redirect("/Events");
         }
-    }
+
+        //get: events/edit/eventId
+        [HttpGet]
+        [Route("/Events/Edit/{eventId?}")]
+        public IActionResult Edit(int eventId)
+        {
+            Event eventById = EventData.GetById(eventId);
+            ViewBag.editEvent = eventById;
+            ViewBag.title = $"Edit Event {eventById.Name}  (id={eventById.Id})";
+            return View();
+        }
+
+        //post: events/edit
+        [HttpPost]
+        [Route("/Events/Edit")]
+        public IActionResult SubmitEditEventForm(int eventId, string name, string description)
+        {
+            Event eventById = EventData.GetById(eventId);
+            eventById.Name = name;
+            eventById.Description = description;
+            return Redirect("/Events");
+
+        }
+}
 }
